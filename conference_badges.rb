b1700b85d8b31f73495a20b28c2d96fc818fcc52
Terrell Murrell <littlerell12@gmail.com>
2020-09-17 19:13:08 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,28 @@
 # Write your code here.
+require `pry`
+
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(people)
+    people.map do |person|
+      badge_maker(person)
+    end
+end
+
+def assign_rooms(people)
+  people.map.with_index(1) do |person, index|
+    "Hello, #{person}! You'll be assigned to room #{index}!"
+  end
+end
+
+def printer(people)
+  batch_badge_creator(people).each do |badge|
+    puts badge
+  end
+
+  assign_rooms(people).each do |assignments|
+    puts assignments
+  end
+end
